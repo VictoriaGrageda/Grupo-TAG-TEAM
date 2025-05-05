20cd783bc802b85929f8da288761ff594880e598
VictoriaGrageda <160803305+VictoriaGrageda@users.noreply.github.com>
2025-05-04 20:48:06 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Geografia from './pages/Geografia';
+import Deportes from './pages/Deportes';
+import Tecnologia from './pages/Tecnologia';
+import Aleatorio from './pages/Aleatorio';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/geografia" element={<Geografia />} />
+        <Route path="/deportes" element={<Deportes />} />
+        <Route path="/tecnologia" element={<Tecnologia />} />
+        <Route path="/aleatorio" element={<Aleatorio />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
 
