c0a7a8cbbc03a5e6250f6d7af0b7004e3bf32b35
VictoriaGrageda <victoriagragedavallejos@gmail.com>
2025-05-26 23:08:48 -0400

    Agregado componente EditorCuestionario con su ruta

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Geografia from './pages/Geografia';
-import Deportes from './pages/Deportes';
-import Tecnologia from './pages/Tecnologia';
-import Aleatorio from './pages/Aleatorio';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Intro from "./pages/Intro"; 
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Geografia from "./pages/Geografia";
+import Deportes from "./pages/Deportes";
+import Tecnologia from "./pages/Tecnologia";
+import Aleatorio from "./pages/Aleatorio";
 import EditorCuestionario from './pages/creacionCuestionarios/EditorCuestionario';
 import VistaCuestionario from './pages/creacionCuestionarios/VistaCuestionario';
 
