6830d81fdbd74f3ba3c03f28c9953afdf43baf57
VcJu4n <danielv0c2@gmail.com>
2025-06-02 22:18:59 -0400

    Update Aleatorio.jsx

--- frontend/src/pages/Aleatorio.jsx
+++ frontend/src/pages/Aleatorio.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ModalDificultad from "../assets/ModalDificultad";
-
+import "../assets/aleatorio.css";
 
 export default function Aleatorio() {
   const [mostrarModal, setMostrarModal] = useState(true);
@@ -14,7 +14,7 @@ export default function Aleatorio() {
   };
 
   const manejarCancelar = () => {
-    navigate("/");
+    navigate("/home");
   };
 
   return (
