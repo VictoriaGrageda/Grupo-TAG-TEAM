2c920702f055cfefaec3c07ff310f3c2aa72f3ed
Brand7i <josepinedogonza@gmail.com>
2025-06-02 23:43:02 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const preguntasRouter = require('./routes/preguntas');
+const cuestionariosRouter = require('./routes/cuestionarios'); 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/preguntas', preguntasRouter);
+app.use('/api/cuestionarios', cuestionariosRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+  console.log(` Servidor corriendo en http://localhost:${PORT}`);
 });
