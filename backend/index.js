d1623004dccc65a291563797a7ff023a18fc264b
Brand7i <josepinedogonza@gmail.com>
2025-06-02 23:40:55 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const preguntasRouter = require('./routes/preguntas');
+const cuestionariosRouter = require('./routes/cuestionarios'); // ESTA ES LA LÍNEA QUE FALTABA
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/preguntas', preguntasRouter);
+app.use('/api/cuestionarios', cuestionariosRouter); //  YA NO FALLA
 
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+  console.log(` Servidor corriendo en http://localhost:${PORT}`);
 });
