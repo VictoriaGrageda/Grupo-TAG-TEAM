09345944f58ef3dc84e1492bc398a4c3cdfd2d77
Brand7i <josepinedogonza@gmail.com>
2025-06-02 23:40:13 -0400

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,16 +2,16 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'tu_usuario',
-  password: 'tu_contraseña',
+  user: 'root',         
+  password: '',        
   database: 'cuestionarios_interactivos',
 });
 
 db.connect((err) => {
   if (err) {
-    console.error('❌ Error de conexión:', err);
+    console.error(' Error de conexión:', err);
   } else {
-    console.log('✅ Conectado a la base de datos');
+    console.log(' Conectado a la base de datos');
   }
 });
 
