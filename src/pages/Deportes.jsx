ad33d2a94bab43440c6c1d1e6b5681ee66ab2620
VcJu4n <danielv0c2@gmail.com>
2025-06-16 12:46:36 -0400

    Add files via upload

---
+++ src/pages/Deportes.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ModalDificultad from "../assets/ModalDificultad";
+
+ 
+
+export default function Deportes() {
+  const [mostrarModal, setMostrarModal] = useState(true);
+  const [dificultad, setDificultad] = useState(null);
+  const navigate = useNavigate();
+
+  const manejarSeleccion = (nivel) => {
+    setDificultad(nivel);
+    setMostrarModal(false);
+  };
+
+  const manejarCancelar = () => {
+    navigate("/home");
+  };
+
+  const renderizarPregunta = () => {
+    if (dificultad === "Fácil");
+    if (dificultad === "Intermedio") ;
+    if (dificultad === "Difícil");
+    return null;
+  };
+
+  return (
+    <div className="Deportes-container">
+      {mostrarModal && (
+        <ModalDificultad onSelect={manejarSeleccion} onClose={manejarCancelar} />
+      )}
+
+      {!mostrarModal && (
+        <>
+          <button className="btn-regresar" onClick={() => navigate("/home")}>
+            ⬅ 
+          </button>
+
+          <h1 className="Deportes-titulo">Categoría: Deportes</h1>
+          <p className="Deportes-subtitulo">
+            Dificultad seleccionada: <strong>{dificultad}</strong>
+          </p>
+
+          {renderizarPregunta()}
+        </>
+      )}
+    </div>
+  );
+}
---
+++ src/pages/tecnologia/preguntas/TecDificil.jsx
@@ -0,0 +1,12 @@
+// src/pages/tecnologia/preguntas/dificil.jsx
+
+import React from "react";
+
+export default function Dificil() {
+  return (
+    <div>
+      <h2>Preguntas Difíciles</h2>
+      {/* Tu contenido */}
+    </div>
+  );
+}
